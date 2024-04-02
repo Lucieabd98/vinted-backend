3d2d954cfba7d102b4ded2626996c64e259832ec
Lucieabd98 <l.abadia@orange.fr>
2024-04-02 16:21:09 +0200

    added good api

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const router = express.Router();
-const stripe = require("stripe")(process.env.STRIPE_API);
+const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 
 router.post("/payment", async (req, res) => {
   try {
